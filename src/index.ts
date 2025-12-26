ffbc9904c1a230b0e6236bc2bd51d27821a998ed
damianof <damianof@users.noreply.github.com>
2025-12-26 10:47:44 -0500

    Using @tenorlab/dashboard-core

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 // file: src/index.ts
-
+// import dashboard-core styles.css so we can re-export as part of this package
+import '@tenorlab/dashboard-core/styles.css'
 // Re-export ONLY types from core for developer convenience
 export type * from '@tenorlab/dashboard-core'
 // export components and react-specific code
