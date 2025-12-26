7c0218abb8a533a9852c24d26bb52881ce58efe9
damianof <damianof@users.noreply.github.com>
2025-12-26 10:42:16 -0500

    Using @tenorlab/dashboard-core

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
 // file: src/index.ts
+
+// Re-export ONLY types from core for developer convenience
+export type * from '@tenorlab/dashboard-core'
+// export components and react-specific code
 export * from './components/'
