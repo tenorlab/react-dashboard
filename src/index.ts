4c5f14692200240e74bc0e9c90eae0dd18b19ab3
damianof <damianof@users.noreply.github.com>
2025-12-26 11:15:39 -0500

    Using @tenorlab/dashboard-core

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,25 @@
 // import dashboard-core styles.css so we can re-export as part of this package
 import '@tenorlab/dashboard-core/styles.css'
 // Re-export ONLY types from core for developer convenience
-export type * from '@tenorlab/dashboard-core'
+export type {
+  IChildWidgetConfigEntry,
+  IDashboardConfig,
+  IDashboardGridPropsBase,
+  IDashboardSettingEntry,
+  IDashboardStorageService,
+  IDashboardWidgetPropsBase,
+  IDynamicWidgetCatalogEntryBase,
+  TDashboardUndoStatus,
+  TDashboardWidgetCatalogBase,
+  TDashboardWidgetKey,
+  TManifestEntry,
+  TUndoHistoryEntry,
+  TWidgetCategory,
+  TWidgetDirection,
+  TWidgetFactoryBase,
+  TWidgetMetaInfoBase,
+  TWidgetSize,
+} from '@tenorlab/dashboard-core'
+
 // export components and react-specific code
 export * from './components/'
